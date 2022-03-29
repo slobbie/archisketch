a22bde964acb32062fc492483b3a7bb867780d2b
slobbie <haeru9410@gmail.com>
2022-03-29 20:58:45 +0900

    :books: last commit

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { useRecoilState, useRecoilValue } from 'recoil';
+import { useRecoilValue } from 'recoil';
 import styled from 'styled-components';
 import { Toggle, Update } from '../atiom';
 import DummayData from '../data/dummyData.json';
